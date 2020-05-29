d187dfc598bfc2369bfd23970b93498a89509bcb
Katarzyna Machura - Mika <machurakatarzyna@gmail.com>
2020-05-29 23:18:48 +0200

    Initial commit. Transformation of MrFeverApp started. Structure of project created. Added MainActivity. Added Firebase database.

---
+++ app/src/main/java/com/kmm/mrfever/IndexActivity.kt
@@ -0,0 +1,14 @@
+package com.kmm.mrfever
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IndexActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_index)
+    }
+
+
+}
